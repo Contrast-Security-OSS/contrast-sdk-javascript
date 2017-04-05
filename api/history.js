461fb1ad204ecc25f305375f621243b192238bf3
Donald Propst <donaldpropst@Donalds-MacBook-Pro.local>
2017-04-05 14:44:10 -0400

    CONTRAST-13458 added history endpoints & tests

---
+++ api/history.js
@@ -0,0 +1,15 @@
+function getOrganizationScoreHistory(orgUuid, limit){
+    var path = `${orgUuid}/history/scores`;
+    return this._get(path, params={'limit':limit});
+}
+
+function getOrganizationScoreHistoryInterval(orgUuid, interval, includeDefense){
+    defense = includeDefense ? '/defense' : '';
+    var path = `${orgUuid}//history/scores/interval${defense}`;
+    return this._get(path, params={'interval':interval});
+}
+
+
+
+module.exports.getOrganizationScoreHistory = getOrganizationScoreHistory;
+module.exports.getOrganizationScoreHistoryInterval = getOrganizationScoreHistoryInterval;
